fa00e5a47ad428aef974dfd4471cf259169586e6
ShubhamPal19 <shubhampal2309@gmail.com>
2021-04-07 19:21:55 +0530

    dynamic web page

--- midas/projects/project1/calc/views.py
+++ midas/projects/project1/calc/views.py
@@ -7,4 +7,12 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return render(request,'home.html')
+    return render(request,'home3.html',{'name': 'Shubham'})
+
+
+def add(request):
+    val1=int(request.GET['num1'])
+    val2=int(request.GET['num2'])
+
+    res=val1+val2
+    return render(request,'result.html',{'result': res })
